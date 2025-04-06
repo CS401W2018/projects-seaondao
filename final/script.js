f65dfacb571a523e6af014fbdc75c0c78dd0469c
Shion Tetuanui <shion120@go.byuh.edu>
2025-04-05 16:06:11 -1000

    Final/First update - Home, Japan,Tahiti,Tahiland

--- final/script.js
+++ final/script.js
@@ -1,1 +1,23 @@
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
 
+
+card1.addEventListener('mouseover',function(){
+    card1.addEventListener('click',function(){
+        card1.classList.add("hide");
+    })    
+})
+card1.addEventListener('mouseleave',function(){
+    card1.classList.remove("hide");
+})
+
+
+
+card2.addEventListener('mouseover',function(){
+    card2.addEventListener('click',function(){
+        card2.classList.add("hide");
+    })    
+})
+card2.addEventListener('mouseleave',function(){
+    card2.classList.remove("hide");
+})
