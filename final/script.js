91f4251d8d62fabf8f78fde44a7e4ca48d44bb90
Shion Tetuanui <shion120@go.byuh.edu>
2025-04-09 17:16:19 -1000

    add Kyoto and form

--- final/script.js
+++ final/script.js
@@ -1,23 +1,38 @@
 const card1 = document.getElementById("card1");
 const card2 = document.getElementById("card2");
 
-
-card1.addEventListener('mouseover',function(){
-    card1.addEventListener('click',function(){
-        card1.classList.add("hide");
-    })    
-})
-card1.addEventListener('mouseleave',function(){
-    card1.classList.remove("hide");
-})
+const form = document.getElementById("form");
 
 
 
-card2.addEventListener('mouseover',function(){
-    card2.addEventListener('click',function(){
-        card2.classList.add("hide");
-    })    
-})
-card2.addEventListener('mouseleave',function(){
-    card2.classList.remove("hide");
+form.addEventListener("submit", function(e){
+    e.preventDefault();//this will prevent from redirecting you to (submitted web)
+    const fName = document.getElementById("fName");
+    const lName = document.getElementById("lName");
+    const email = document.getElementById("email");
+    const where = document.getElementById("where");
+    const values = {
+        fName:fName.value,
+        lName:lName.value,
+        email:email.value,
+        where:where.value,
+    }
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("Get","mock.json",true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8");
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState === 4 && xhr.status === 200){
+            const response = JSON.parse(xhr.responseText);
+            alert('Form submitted successfully');
+            document.getElementById("message").innerHTML = response.message;
+            document.getElementById("form").innerHTML = "";
+        }else if (xhr.readyState === 4){
+            alert("Error Submitted form")
+        }
+    };
+
+    xhr.send(JSON.stringify(values));
+    alert(`Success: ${values.fName}`);
+    
 })
